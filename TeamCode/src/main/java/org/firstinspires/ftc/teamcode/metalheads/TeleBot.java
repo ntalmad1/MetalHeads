6d570c0ef05407e1d5096af44b85fdd0d9e09228
IsaacT <isaactalmadge@gmail.com>
2025-02-01 12:31:42 -0800

    Initial Pose update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/TeleBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalheads/TeleBot.java
@@ -41,6 +41,6 @@ public class TeleBot extends CompBot {
         super.configureBot();
 
         // initialize roadrunner from last op pose
-        this.initialPose = DriveTrainPositionStruct.pose == null ? new Pose2d(0, 0, 0) : DriveTrainPositionStruct.pose;
+        this.initialPose = DriveTrainPositionStruct.pose == null ? new Pose2d(8, -61, Math.toRadians(90)) : DriveTrainPositionStruct.pose;
     }
 }
